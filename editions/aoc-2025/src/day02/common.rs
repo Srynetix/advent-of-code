176445de28c5db7922ef1d83ff0668eb0c1e05eb
Denis BOURGE <bourge.denis@gmail.com>
2025-12-03 06:59:05 +0000

    feat: 2025 day 02

--- editions/aoc-2025/src/day02/common.rs
+++ editions/aoc-2025/src/day02/common.rs
@@ -1,1 +1,132 @@
 //! Common
+
+use std::str::FromStr;
+
+use itertools::Itertools;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct ProductIdRange {
+    pub start: usize,
+    pub end: usize,
+}
+
+pub struct ProductIdRanges(pub Vec<ProductIdRange>);
+
+impl FromStr for ProductIdRanges {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let ranges = s
+            .split(',')
+            .map(|part| part.parse::<ProductIdRange>().unwrap())
+            .collect();
+        Ok(ProductIdRanges(ranges))
+    }
+}
+
+impl FromStr for ProductIdRange {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split('-').collect();
+        if parts.len() != 2 {
+            panic!("Invalid range format");
+        }
+        let start = parts[0].parse::<usize>().unwrap();
+        let end = parts[1].parse::<usize>().unwrap();
+        Ok(ProductIdRange { start, end })
+    }
+}
+
+pub fn check_invalid_patterns(range: ProductIdRange) -> Vec<usize> {
+    let mut patterns = Vec::new();
+
+    for product_id in range.start..=range.end {
+        let id_str = product_id.to_string();
+        let (before, after) = id_str.split_at(id_str.len() / 2);
+
+        if before == after {
+            patterns.push(product_id);
+        }
+    }
+
+    patterns
+}
+
+pub fn check_invalid_patterns_complex(range: ProductIdRange) -> Vec<usize> {
+    let mut patterns = Vec::new();
+
+    'products: for product_id in range.start..=range.end {
+        let id_str = product_id.to_string();
+        let len = id_str.len();
+
+        'search: for window_size in 1..=(len / 2) {
+            let chunks = id_str
+                .chars()
+                .chunks(window_size)
+                .into_iter()
+                .map(|chunk| chunk.collect::<String>())
+                .collect::<Vec<String>>();
+            for i in 1..chunks.len() {
+                if chunks[i - 1] != chunks[i] {
+                    continue 'search;
+                }
+            }
+
+            patterns.push(product_id);
+            continue 'products;
+        }
+    }
+
+    patterns
+}
+
+pub fn check_invalid_patterns_many(ranges: &ProductIdRanges) -> usize {
+    ranges
+        .0
+        .iter()
+        .map(|range| check_invalid_patterns(*range).into_iter().sum::<usize>())
+        .sum()
+}
+
+pub fn check_invalid_patterns_complex_many(ranges: &ProductIdRanges) -> usize {
+    ranges
+        .0
+        .iter()
+        .map(|range| {
+            check_invalid_patterns_complex(*range)
+                .into_iter()
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_product_id_range() {
+        let range_str = "100-200";
+        let range = range_str.parse::<ProductIdRange>().unwrap();
+        assert_eq!(range.start, 100);
+        assert_eq!(range.end, 200);
+    }
+
+    #[test]
+    fn test_check_invalid_patterns() {
+        assert_eq!(check_invalid_patterns("11-22".parse().unwrap()), &[11, 22]);
+
+        assert_eq!(check_invalid_patterns("95-115".parse().unwrap()), &[99]);
+
+        assert_eq!(check_invalid_patterns("998-1012".parse().unwrap()), &[1010]);
+    }
+
+    #[test]
+    fn test_check_invalid_patterns_complex() {
+        assert_eq!(
+            check_invalid_patterns_complex("998-1012".parse().unwrap()),
+            &[999, 1010]
+        );
+    }
+}
--- editions/aoc-2025/src/day02/mod.rs
+++ editions/aoc-2025/src/day02/mod.rs
@@ -41,6 +41,31 @@
 //! Adding up all the invalid IDs in this example produces <code><em>1227775554</em></code>.
 //!
 //! <em>What do you get if you add up all of the invalid IDs?</em>
+//!
+//! # --- Part Two ---
+//!
+//! The clerk quickly discovers that there are still invalid IDs in the ranges in your list. Maybe the young Elf was doing other silly patterns as well?
+//!
+//! Now, an ID is invalid if it is made only of some sequence of digits repeated <em>at least</em> twice. So, <code>12341234</code> (<code>1234</code> two times), <code>123123123</code> (<code>123</code> three times), <code>1212121212</code> (<code>12</code> five times), and <code>1111111</code> (<code>1</code> seven times) are all invalid IDs.
+//!
+//! From the same example as before:
+//!
+//! - 11-22 still has two invalid IDs, 11 and 22.
+//! - 95-115 now has two invalid IDs, 99 and 111.
+//! - 998-1012 now has two invalid IDs, 999 and 1010.
+//! - 1188511880-1188511890 still has one invalid ID, 1188511885.
+//! - 222220-222224 still has one invalid ID, 222222.
+//! - 1698522-1698528 still contains no invalid IDs.
+//! - 446443-446449 still has one invalid ID, 446446.
+//! - 38593856-38593862 still has one invalid ID, 38593859.
+//! - 565653-565659 now has one invalid ID, 565656.
+//! - 824824821-824824827 now has one invalid ID, 824824824.
+//! - 2121212118-2121212124 now has one invalid ID, 2121212121.
+//!
+//!
+//! Adding up all the invalid IDs in this example produces <code><em>4174379265</em></code>.
+//!
+//! <em>What do you get if you add up all of the invalid IDs using these new rules?</em>
 
 pub mod common;
 pub mod part1;
